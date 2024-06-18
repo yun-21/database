9b3d942b724e8774840ae3fd4822a8df26aaa757
jiyun <ranji12@naver.com>
2024-06-18 23:48:42 +0900

    Error: Cannot find module 'better-sqlite3'

--- app.js
+++ app.js
@@ -2,4 +2,19 @@
 //better-sqlite3는 동기적으로 작동한다는 특징이 있다.
 const db = require("better-sqlite3");
 
-const database = new db.database("./test.db")
+class MakeDatabase {
+    constructor(){
+        this.database = new db("./test.db");
+    }
+    createData(){
+        this.database.run("CREATE TABLE smart(id TEXT PRIMARY KEY, pwd TEXT, age INTEGER)");
+    }
+
+    insertData(id ,pwd ,age){
+        let insert = this.database.prepare("INSERT INTO smart(id, pwd, age) value (?,?,?)");
+        insert.run(id,pwd,age)
+        insert.finalize()
+    }
+}
+const make = new MakeDatabase();
+make.insertData("신지윤","신지윤123",25)
