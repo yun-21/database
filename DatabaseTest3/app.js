f616892f77775a9242436687bb7b9a3f4330ba95
shinjiyun <ranji12@naver.com>
2024-06-20 11:50:40 +0900

    insert.run(obj)에서 오류가 났음 obj.name 이렇게 지정을 안 해줘서인 것같음

--- DatabaseTest3/app.js
+++ DatabaseTest3/app.js
@@ -4,12 +4,27 @@ const db = new database("./test.db");
 const CreateTable = (tableName) =>{
   const check = db.prepare(`SELECT COUNT(*) FROM sqlite_master WHERE name=?`)
   const isTrue = Object.values(check.get(tableName))*1
-  // if(isTrue){
-  //   console.log(`${tableName}의 테이블이 이미 있음`)
-  // }
-  // else{
-  //   check.run()
-  // }
-  console.log(isTrue);
+  if(isTrue){
+    console.log(`${tableName}의 테이블이 이미 있음`)
+  }
+  else{
+    const create = db.prepare(`CREATE TABLE ${tableName}(id INTEGER PRIMARY KEY, name TEXT NOT NULL, age INTEGER NOT NULL)`);
+    create.run()
+  }
 }
+
+const InsertData = (obj, tableName) => {
+  const insert = db.prepare(`INSERT INTO ${tableName} (id, name, age) VALUES(?,?,?)`)
+  insert.run(obj);
+  insert.finalize();
+}
+let obj = {
+  id : 1,
+  name : "객체",
+  age : 100
+}
+
+InsertData(obj,"test")
+
 CreateTable("test");
+
