1e2389356680257b9dcfa8a06ca4526d359342d2
shinjiyun <ranji12@naver.com>
2024-06-20 09:29:42 +0900

    createData변수에 할당된 create테이블에서 )로 닫아주지않아서 오류가 났었다. => 해결!

--- DatabaseTest2/app.js
+++ DatabaseTest2/app.js
@@ -1,16 +1,32 @@
 const database = require('better-sqlite3');
 const db = new database("./better.db");
+// const database = require('better-sqlite3');
+// const db = new database("./better.db");
 
 const checkDb = db.prepare("SELECT COUNT(*) FROM sqlite_master WHERE name='zootopia'");
+// const checkDb = db.prepare("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='zootopia'");
 
 //Object.values() 정적 메서드는 지정된 객체 자체의 열거 가능한 문자열 키 속성 값의 배열을 반환합니다.
 let isTrue = Object.values(checkDb.get())*1;
 if(isTrue){
-    console.log("dd")
+    console.log("테이블이 이미 존재합니다.");
 }
 else{
-    console.log("ss")
-    const createData = db.prepare("CREATE TABLE zootopia(name TEXT PRIMARY KEY, age INTEGER NOT NULL, address TEXT")
+    const createData = db.prepare("CREATE TABLE zootopia (name TEXT PRIMARY KEY, age INTEGER NOT NULL, address TEXT)")
     createData.run();
-    db.close()
+    console.log("테이블을 생성했습니다.");
 }
+
+const insertData = ()=>{
+    const insert = db.prepare("INSERT INTO zootopia (name, age, address) VALUES ('신지윤', 25, '태평동')");
+    insert.run()
+}
+if(isTrue){
+    insertData()
+}
+db.close()
+
+
+
+
+
