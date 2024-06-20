843467ad6337ba847d9033b25e8c3e84c3d6bc0f
shinjiyun <ranji12@naver.com>
2024-06-20 12:08:02 +0900

    createTable파일을 불러와서 함수 실행시켰음

--- DatabaseTest3/app.js
+++ DatabaseTest3/app.js
@@ -1,31 +1,22 @@
-const database = require("better-sqlite3");
-const db = new database("./test.db");
 const create = require("./module/createTable")
 
+// const UpdateData = (obj, tableName) =>{
+//   const update = db.prepare(`UPDATE ${tableName} SET name = ? WHERE id = ?`)
+//   update.run("바이", obj.id)
+// }
 
-const InsertData = (obj, tableName) => {
-  const insert = db.prepare(`INSERT INTO ${tableName} (id, name, age) VALUES(?,?,?)`)
-  insert.run(obj.id, obj.name, obj.age);
-  // insert.finalize(); 
-}
+// const deleteData = (name, tableName) => {
+//   const delets = db.prepare(`DELETE FROM ${tableName} WHERE name = ?`)
+//   delets.run(name)
+// }
 
-const UpdateData = (obj, tableName) =>{
-  const update = db.prepare(`UPDATE ${tableName} SET name = ? WHERE id = ?`)
-  update.run("바이", obj.id)
-}
-
-const deleteData = (name, tableName) => {
-  const delets = db.prepare(`DELETE FROM ${tableName} WHERE name = ?`)
-  delets.run(name)
-}
-
-let obj = {
-  id : 3,
-  name : "뭐지",
-  age : 103
-}
+// let obj = {
+//   id : 3,
+//   name : "뭐지",
+//   age : 103
+// }
 
 // InsertData(obj,"test")
-UpdateData(obj,"test")
+// UpdateData(obj,"test")
 create("mango");
-deleteData("배열","test")
+// deleteData("배열","test")
