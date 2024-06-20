5bec5f7250cd2350d6702a905eaaabf12b6aab18
shinjiyun <ranji12@naver.com>
2024-06-20 12:49:24 +0900

    delete 모듈 추가했다.

--- DatabaseTest3/app.js
+++ DatabaseTest3/app.js
@@ -1,21 +1,16 @@
 const create = require("./module/createTable")
 const insert = require("./module/insertData")
 const update = require("./module/updateData")
-
-
-// const deleteData = (name, tableName) => {
-//   const delets = db.prepare(`DELETE FROM ${tableName} WHERE name = ?`)
-//   delets.run(name)
-// }
+const deletes = require("./module/deleteData")
 
 let obj = {
   id : 3,
-  name : "뭐지",
+  name : "하구",
   age : 103
 }
 
-update("바바",obj,"mango","name","id")
-create("mango");
+// update("바바",obj,"mango","name","id")
+// create("mango");
 // insert(obj,"mango")
 
-// deleteData("배열","test")
+deletes("정구","mango")
